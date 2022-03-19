270e63dc3855e4d792f56bbfca8d1a07da18d51e
KyoungSeok Moon <rudtjr0705@naver.com>
2022-03-19 22:38:05 +0900

    Update paging & board-read

--- src/main/java/com/moon/aza/controller/BoardController.java
+++ src/main/java/com/moon/aza/controller/BoardController.java
@@ -13,34 +13,35 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Log4j2
 @RequiredArgsConstructor
+@RequestMapping("/board")
 @Controller
 public class BoardController {
     private final BoardService boardService;
 
 
     /* 글 작성 페이지 */
-    @GetMapping("/board/register")
-    public String boardRegister(@CurrentMember Member member, Model model){
+    @GetMapping("/register")
+    public void boardRegister(@CurrentMember Member member, Model model){
         log.info("/board/register");
         model.addAttribute("member",member);
         model.addAttribute(new BoardForm());
-        return "/aza/board-register";
     }
-    @GetMapping("/board/read")
-    public String boardRead(@CurrentMember Member member, long id, PageRequestDTO pageRequestDTO, Model model){
-        log.info("/board/read");
+
+    @GetMapping({"/read","/modify"})
+    public void boardRead(@CurrentMember Member member, Long id, PageRequestDTO pageRequestDTO, Model model){
+        log.info("id : "+id);
         BoardForm boardForm = boardService.read(id);
         model.addAttribute("boardForm", boardForm);
         if(member != null)
             model.addAttribute(member);
-        return "/aza/board-read";
     }
 
-    @PostMapping("/board/register")
+    @PostMapping("/register")
     public String register(@ModelAttribute BoardForm boardForm, RedirectAttributes redirectAttributes){
         Board board = boardService.register(boardForm);
 
